347acea8418bc0010935cf3792b696bb34bf0607
Pankajjangid01 <130858050+Pankajjangid01@users.noreply.github.com>
2025-02-07 14:28:58 +0530

    Added All custom modules

---
+++ odoo-traning-dev/custom_addons/new_module/models/student_details.py
@@ -0,0 +1,58 @@
+from odoo import models, fields,api
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class StudentDetails(models.Model):
+    _name = "student.student"
+    _description = "Student Details"
+
+    phone = fields.Integer("Phone")
+    name = fields.Char(string="Name")
+    age = fields.Integer(string="Age")
+    email = fields.Char(string="Email", unique=True)
+    branch = fields.Selection([
+        ('cse', 'Computer Science'),
+        ('ece', 'Electronics and Communication'),
+        ('mech', 'Mechanical Engineering'),
+        ('civil', 'Civil Engineering'),
+        ('it', 'Information Technology'),
+    ], string="Branch")
+    college_name = fields.Char(string="College Name")
+    student_id = fields.Char(string="Student ID")
+    active = fields.Boolean(string="Active", default=True)
+    start_date = fields.Date(string="Date")
+    course_name = fields.Char(string="Course Name")
+    course_amount = fields.Float(string="Course amount")
+    course_description = fields.Html(string="Course Description")
+    course_professor_name = fields.Char(string="Professor Name")
+    course_professor_contact = fields.Integer(string="Professor Contact")
+
+    @api.model
+    def create_record(self, name, age, email, student_id, college_name, branch):
+        _logger.debug("All okay in debugging")
+        return self.create({
+            'name': name,
+            'age': age,
+            'email': email,
+            'student_id': student_id,
+            'college_name': college_name,
+            'branch': branch,
+        })
+
+
+    @api.model
+    def write_record(self,id,email,college_name):
+        searched_record = self.search([('id', '=', id)])
+        if searched_record:
+            searched_record.write(dict(email=email,college_name=college_name))
+            _logger.info("Searched Record Information",searched_record)
+        else:
+            _logger.error("No record found with given id",searched_record)
+
+    @api.model
+    def delete_record(self,id):
+        delete_data = self.search([('id', '=', id)])
+        if delete_data:
+            delete_data.unlink()
+
---
+++ odoo-traning-dev/custom_addons/new_module/tests/test_create_record.py
@@ -0,0 +1,35 @@
+from odoo.tests import TransactionCase
+
+class TestCreateRecord(TransactionCase):
+
+    def setUp(self):
+        super(TestCreateRecord, self).setUp()
+
+        student = self.env['student.student'].create({
+            'name': 'Anuj',
+            'age': 22,
+            'email': 'anuj@example.com',
+            'student_id': '12345',
+            'college_name': 'XYZ College',
+            'branch': 'cse',
+        })
+
+        self.student_details = student.create_record(
+            name='Anuj',
+            age=22,
+            email='anuj@example.com',
+            student_id='12345',
+            college_name='XYZ College',
+            branch='cse'
+        )
+
+    def test_create_record(self):
+        import pdb;
+        pdb.set_trace()
+        self.assertEqual(self.student_details.name, 'Anuj')
+
+    def test_create_record_2(self):
+        self.assertNotEqual(self.student_details.age, 20)
+
+    def test_create_record_3(self):
+        self.assertIsNot(self.student_details.name, self.student_details.age)
