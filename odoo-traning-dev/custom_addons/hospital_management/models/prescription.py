23092dc6aa50ff873f1bac4233c0a159b375c3ac
Pankajjangid01 <130858050+Pankajjangid01@users.noreply.github.com>
2025-02-26 13:33:20 +0530

    Update prescription.py

--- odoo-traning-dev/custom_addons/hospital_management/models/prescription.py
+++ odoo-traning-dev/custom_addons/hospital_management/models/prescription.py
@@ -1,16 +1,16 @@
-from odoo import models,fields
+from odoo import models, fields, api
 
 class Prescription(models.Model):
-
-    _name="prescription.prescription"
+    _name = "prescription.prescription"
     _description = "Prescription model to manage the prescription of the patient"
 
-    patient_id = fields.Many2one("patient.patient",string="Patient")
+    patient_id = fields.Many2one("patient.patient", string="Patient")
     prescription_date = fields.Datetime(string="Prescription Date")
-    pharmacy = fields.Many2one("appointment.appointment",string="Pharmacy")
-    prescription_id = fields.Char(string="Prescription Id",compute="_compute_prescription_id")
-    login_user = fields.Char(string="Login User",compute="_compute_login_user")
-    prescribing_doctor = fields.Many2one("patient.patient",string="Prescribing Doctor")
+    appointment_id = fields.Many2one("appointment.appointment", string="Appointment")  # Link to Appointment
+    health_center = fields.Selection(related="appointment_id.health_center", string="Health Center", store=True, readonly=True)  # Fetch Health Center
+    prescription_id = fields.Char(string="Prescription Id", compute="_compute_prescription_id")
+    login_user = fields.Char(string="Login User", compute="_compute_login_user")
+    prescribing_doctor = fields.Many2one("patient.patient", string="Prescribing Doctor")
     invoice_to_insurance = fields.Boolean(string="Invoice to Insurance")
     prescription_details = fields.Many2many("medicine.medicine")
 
@@ -21,5 +21,5 @@ class Prescription(models.Model):
                 record.prescription_id = f"PRES2017/{record.id}"
 
     def _compute_login_user(self):
-        """Function to compute Prescription ID"""
+        """Function to compute Login User"""
         self.login_user = self.env.user.name
