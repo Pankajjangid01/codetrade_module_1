6f73a885e88b538f4985502d9caf82939eda5ac6
Pankajjangid01 <130858050+Pankajjangid01@users.noreply.github.com>
2025-03-03 13:36:33 +0530

    Update lab_result_wizard.py

--- hospital_management/wizard/lab_result_wizard.py
+++ hospital_management/wizard/lab_result_wizard.py
@@ -1,20 +1,23 @@
-from odoo import models
+from odoo import models, fields
 
 class LabResult(models.TransientModel):
-    
     _name = "lab.result"
     _description = "Create the lab test result"
 
-    def confirm(self):
-        """This function confirm and close the wizard window"""
-        return {
-                'name': ('Lab Test Result Info'),
-                'type': 'ir.actions.act_window',
-                'res_model': 'lab.test.info',
-                'view_mode': 'list,form',
-                }
+    laboratory_id = fields.Many2one("draft.laboratory", string="Lab Test")
 
-    @staticmethod    
-    def cancel():
-        """This function close the wizard window"""
+    def confirm(self):
+        """Confirm test and mark as Tested"""
+        if self.laboratory_id:
+            self.laboratory_id.state = 'tested'  # Update state in the main record
+
+        return {
+            'name': 'Lab Test Result Info',
+            'type': 'ir.actions.act_window',
+            'res_model': 'lab.test.info',
+            'view_mode': 'list,form',
+        }
+
+    def cancel(self):
+        """Close wizard without changes"""
         return {'type': 'ir.actions.act_window_close'}
