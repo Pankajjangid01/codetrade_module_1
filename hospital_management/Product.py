e4d6e191447559ec569fed3723ff8da7b49c22e7
Pankajjangid01 <130858050+Pankajjangid01@users.noreply.github.com>
2025-03-12 15:06:10 +0530

    Update Product.py

--- hospital_management/Product.py
+++ hospital_management/Product.py
@@ -1,101 +1,81 @@
-# models/product_import_wizard.py
+<!-- my_module/views/product_template_kanban_button.xml -->
+<odoo>
+    <record id="product_template_kanban_view_custom" model="ir.ui.view">
+        <field name="name">product.template.kanban.custom</field>
+        <field name="model">product.template</field>
+        <field name="inherit_id" ref="product.product_template_kanban_view"/>
+        <field name="arch" type="xml">
+            <xpath expr="//kanban" position="attributes">
+                <attribute name="create">true</attribute>
+            </xpath>
+            <xpath expr="//kanban/templates" position="before">
+                <header>
+                    <button type="action"
+                            name="%(action_product_excel_upload_wizard)d"
+                            string="Upload Products (Excel)"
+                            class="btn btn-primary"
+                            context="{}"/>
+                </header>
+            </xpath>
+        </field>
+    </record>
+</odoo>
+# my_module/models/product_excel_upload_wizard.py
 
 from odoo import models, fields, api
 import base64
-import io
-import xlrd  # For .xls
-import openpyxl  # For .xlsx
+import tempfile
+import openpyxl
 
-class ProductImportWizard(models.TransientModel):
-    _name = 'product.import.wizard'
-    _description = 'Import Products from Excel'
+class ProductExcelUploadWizard(models.TransientModel):
+    _name = 'product.excel.upload.wizard'
+    _description = 'Product Excel Upload Wizard'
 
-    file = fields.Binary(string='Excel File', required=True)
-    file_name = fields.Char(string='File Name')
+    upload_file = fields.Binary("Upload Excel File", required=True)
+    file_name = fields.Char("File Name")
 
-    def import_products(self):
-        if not self.file_name.endswith('.xlsx'):
-            raise UserError("Please upload a .xlsx file")
+    def action_upload_excel(self):
+        if self.upload_file:
+            file_data = base64.b64decode(self.upload_file)
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx") as f:
+                f.write(file_data)
+                f.seek(0)
+                workbook = openpyxl.load_workbook(f.name)
+                sheet = workbook.active
 
-        file_data = base64.b64decode(self.file)
-        workbook = openpyxl.load_workbook(io.BytesIO(file_data))
-        sheet = workbook.active
+                for row in sheet.iter_rows(min_row=2, values_only=True):
+                    name = row[0]
+                    price = row[1] if len(row) > 1 else 0.0
+                    self.env['product.template'].create({
+                        'name': name,
+                        'list_price': price,
+                    })
 
-        for row in sheet.iter_rows(min_row=2, values_only=True):  # Skipping header row
-            name, default_code, price, qty, uom_name = row[:5]
+        return {'type': 'ir.actions.act_window_close'}
 
-            # Find existing product
-            product = self.env['product.template'].search([('default_code', '=', default_code)], limit=1)
-
-            # Find UoM
-            uom = self.env['uom.uom'].search([('name', '=', uom_name)], limit=1)
-
-            vals = {
-                'name': name,
-                'default_code': default_code,
-                'list_price': price or 0.0,
-                'uom_id': uom.id if uom else False,
-                'uom_po_id': uom.id if uom else False,
-            }
-
-            if product:
-                product.write(vals)
-            else:
-                self.env['product.template'].create(vals)
-<!-- views/product_import_wizard_view.xml -->
+<!-- my_module/views/product_excel_upload_wizard_view.xml -->
 <odoo>
-  <record id="view_product_import_wizard" model="ir.ui.view">
-    <field name="name">product.import.wizard.form</field>
-    <field name="model">product.import.wizard</field>
-    <field name="arch" type="xml">
-      <form string="Import Products">
-        <group>
-          <field name="file" filename="file_name"/>
-        </group>
-        <footer>
-          <button string="Upload" type="object" name="import_products" class="btn-primary"/>
-          <button string="Cancel" class="btn-secondary" special="cancel"/>
-        </footer>
-      </form>
-    </field>
-  </record>
-</odoo>
+    <record id="view_product_excel_upload_wizard_form" model="ir.ui.view">
+        <field name="name">product.excel.upload.wizard.form</field>
+        <field name="model">product.excel.upload.wizard</field>
+        <field name="arch" type="xml">
+            <form string="Upload Excel Sheet">
+                <group>
+                    <field name="upload_file" filename="file_name"/>
+                    <field name="file_name"/>
+                </group>
+                <footer>
+                    <button name="action_upload_excel" string="Upload" type="object" class="btn-primary"/>
+                    <button string="Cancel" special="cancel"/>
+                </footer>
+            </form>
+        </field>
+    </record>
 
-<!-- server_action.xml -->
-<odoo>
-  <record id="action_import_product_wizard" model="ir.actions.act_window">
-    <field name="name">Import Products</field>
-    <field name="res_model">product.import.wizard</field>
-    <field name="view_mode">form</field>
-    <field name="target">new</field>
-  </record>
-
-  <record id="server_action_import_product" model="ir.actions.server">
-    <field name="name">Import Products from Excel</field>
-    <field name="model_id" ref="product.model_product_template"/>
-    <field name="binding_model_id" ref="product.model_product_template"/>
-    <field name="state">code</field>
-    <field name="code">
-      action = env.ref('your_module_name.action_import_product_wizard').read()[0]
-    </field>
-  </record>
-</odoo>
-
-<!-- add_button_product_template_view.xml -->
-<odoo>
-  <record id="product_template_tree_view_inherit" model="ir.ui.view">
-    <field name="name">product.template.tree.inherit</field>
-    <field name="model">product.template</field>
-    <field name="inherit_id" ref="product.product_template_tree_view"/>
-    <field name="arch" type="xml">
-      <xpath expr="//tree" position="before">
-        <header>
-          <button name="%(your_module_name.server_action_import_product)d"
-                  string="Import Products"
-                  type="action"
-                  class="btn-primary"/>
-        </header>
-      </xpath>
-    </field>
-  </record>
+    <record id="action_product_excel_upload_wizard" model="ir.actions.act_window">
+        <field name="name">Upload Product Excel</field>
+        <field name="res_model">product.excel.upload.wizard</field>
+        <field name="view_mode">form</field>
+        <field name="target">new</field>
+    </record>
 </odoo>
