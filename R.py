3cb7d1f796b45f10c34eb76effffff77cf0e8365
Pankajjangid01 <130858050+Pankajjangid01@users.noreply.github.com>
2025-04-02 11:44:20 +0530

    Update R.py

--- R.py
+++ R.py
@@ -1,53 +1,43 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<template xml:space="preserve">
-    <t t-name="filter_assessment_button">
-        <button t-if='widget.modelName == "hms.patient"' type="button" class="btn btn-secondary oe_new_custom_button">
-            Patient Assessments
-        </button>
-    </t>
-    <t t-extend="KanbanView.buttons">
-        <t t-jquery="button[class='open_custom_wizard']" t-operation="after">
-            <t t-call="filter_assessment_button" />
-        </t>
-    </t>
-</template>
-<?xml version="1.0" encoding="utf-8"?>
 <odoo>
     <data>
+        <!-- JavaScript and CSS Assets -->
         <template id="assets_backend" name="saned_custome_assets" inherit_id="web.assets_backend">
             <xpath expr="." position="inside">
                 <script type="text/javascript" src="/saned_custome/static/src/js/add_assessment_button.js"></script>
-            </xpath>
-        </template>
-    </data>
-</odoo>
-<?xml version="1.0" encoding="UTF-8"?>
-<template xml:space="preserve">
-    <t t-name="verified_patient_button">
-        <button t-if='widget and widget.verified_patient or widget and widget.verified_case_patient' type="button" class="btn btn-secondary open_custom_wizard">
-            Verify Patient
-        </button>
-    </t>
-    <t t-extend="KanbanView.buttons">
-        <t t-jquery="button" t-operation="after">
-            <t t-call="verified_patient_button" />
-        </t>
-    </t>
-    <t t-extend="ListView.buttons">
-        <t t-jquery="div.o_list_buttons" t-operation="append">
-            <t t-call="verified_patient_button"/>
-        </t>
-    </t>
-</template>
-<?xml version="1.0" encoding="utf-8"?>
-<odoo>
-
-        <template id="assets_backend" inherit_id="web.assets_backend">
-            <xpath expr="." position="inside">
                 <script type="text/javascript" src="/saned_patient_details/static/src/js/vertical_page.js"/>
                 <script type="text/javascript" src="/saned_patient_details/static/src/js/custom_button.js"/>
                 <link rel="stylesheet" type="text/css" href="/saned_patient_details/static/src/css/vertical_pages.css"/>
             </xpath>
         </template>
-
+        
+        <!-- Verify Patient Button -->
+        <template id="verified_patient_button" name="Verified Patient Button">
+            <button t-if='widget and (widget.verified_patient or widget.verified_case_patient)' type="button" class="btn btn-secondary open_custom_wizard">
+                Verify Patient
+            </button>
+        </template>
+        
+        <!-- Patient Assessments Button -->
+        <template id="filter_assessment_button" name="Patient Assessment Button">
+            <button t-if='widget.modelName == "hms.patient"' type="button" class="btn btn-secondary oe_new_custom_button">
+                Patient Assessments
+            </button>
+        </template>
+        
+        <!-- Extend KanbanView.buttons -->
+        <template inherit_id="KanbanView.buttons">
+            <xpath expr="//button[@class='open_custom_wizard']" position="after">
+                <t t-call="filter_assessment_button"/>
+            </xpath>
+        </template>
+        
+        <!-- Extend ListView.buttons -->
+        <template inherit_id="ListView.buttons">
+            <xpath expr="//div[@class='o_list_buttons']" position="append">
+                <t t-call="verified_patient_button"/>
+                <t t-call="filter_assessment_button"/>
+            </xpath>
+        </template>
+    </data>
 </odoo>
