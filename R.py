17915cc3a250e2688a49804c4981fa55dacaaaf6
Pankajjangid01 <130858050+Pankajjangid01@users.noreply.github.com>
2025-04-03 17:47:28 +0530

    Update R.py

--- R.py
+++ R.py
@@ -1,43 +1,40 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<odoo>
-    <data>
-        <!-- JavaScript and CSS Assets -->
-        <template id="assets_backend" name="saned_custome_assets" inherit_id="web.assets_backend">
-            <xpath expr="." position="inside">
-                <script type="text/javascript" src="/saned_custome/static/src/js/add_assessment_button.js"></script>
-                <script type="text/javascript" src="/saned_patient_details/static/src/js/vertical_page.js"/>
-                <script type="text/javascript" src="/saned_patient_details/static/src/js/custom_button.js"/>
-                <link rel="stylesheet" type="text/css" href="/saned_patient_details/static/src/css/vertical_pages.css"/>
-            </xpath>
-        </template>
-        
-        <!-- Verify Patient Button -->
-        <template id="verified_patient_button" name="Verified Patient Button">
-            <button t-if='widget and (widget.verified_patient or widget.verified_case_patient)' type="button" class="btn btn-secondary open_custom_wizard">
-                Verify Patient
-            </button>
-        </template>
-        
-        <!-- Patient Assessments Button -->
-        <template id="filter_assessment_button" name="Patient Assessment Button">
-            <button t-if='widget.modelName == "hms.patient"' type="button" class="btn btn-secondary oe_new_custom_button">
-                Patient Assessments
-            </button>
-        </template>
-        
-        <!-- Extend KanbanView.buttons -->
-        <template inherit_id="KanbanView.buttons">
-            <xpath expr="//button[@class='open_custom_wizard']" position="after">
-                <t t-call="filter_assessment_button"/>
-            </xpath>
-        </template>
-        
-        <!-- Extend ListView.buttons -->
-        <template inherit_id="ListView.buttons">
-            <xpath expr="//div[@class='o_list_buttons']" position="append">
-                <t t-call="verified_patient_button"/>
-                <t t-call="filter_assessment_button"/>
-            </xpath>
-        </template>
-    </data>
-</odoo>
+import { Component, xml,useState } from "@odoo/owl";
+import { registry } from "@web/core/registry";
+
+class TimerWidget extends Component {
+    static template = "custom_timer_widget";
+
+    countDownDate = new Date("Jan 5, 2030 15:37:25").getTime();
+    x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+    }, 1000);
+}
+
+export const timer_id = {
+    component:TimerWidget,
+    supportedType:["char"]
+}
+
+registry.category("fields").add("custom_timer_widget", timer_id);
+<templates>
+    <t t-name="custom_timer_widget">
+        <div class="stopwatch">
+            <div id="display">00:00:00</div>
+            <button id="startBtn">Start</button>
+            <button id="stopBtn">Stop</button>
+            <button id="resetBtn">Reset</button>
+        </div>
+    </t>
+</templates>
+i want to set the timer value and then the timer will start from there how can i do it in odoo 18 using js 
